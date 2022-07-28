5b9b10b857364f069da56325f3fdf896667dda9e
Huynh Minh Nhut <minhnhut2804@gmail.com>
2022-07-29 00:19:25 +0700

    Page UI

---
+++ interfaces/pageUIs/nopCommerce/user/CustomerInforPageUI.java
@@ -0,0 +1,10 @@
+package pageUIs.nopCommerce.user;
+
+public class CustomerInforPageUI {
+	public static final String CUSTOMER_INFOR_HEADER ="xpath=//div[@class='page-title']/h1[text()='My account - Customer info']";
+	//public static final String ADDRESS_LINK ="//div[contains(@class,'account-navigation')]//a[text()='Addresses']";
+	public static final String GENDRER_RADIO_BUTTON ="xpath=//div[@class='gender']//label[text()='%s']";
+	public static final String DYNAMIC_INPUT_FIELD_AT_CUSTOMER_PAGE ="xpath=//div[@class='inputs']//input[@id='%s']";
+	public static final String INPUT_DATE_OF_BIRTH ="xpath=//div[@class='inputs date-of-birth']//select[@name='%s']";
+	public static final String BUTTON_SAVE="name=save-info-button"; 
+}
